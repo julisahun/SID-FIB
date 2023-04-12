d1a51e545f30ff69a11c4e3d6310179152b681ff
juli.sahun <juli.sahun@estudiantat.upc.edu>
2023-04-12 16:49:37 +0200

    working version

--- src/main/java/eu/su/mas/dedaleEtu/mas/agents/dummies/sid/AgentB.java
+++ src/main/java/eu/su/mas/dedaleEtu/mas/agents/dummies/sid/AgentB.java
@@ -15,190 +15,215 @@ import jade.lang.acl.ACLMessage;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
 import java.util.Random;
 
 
 public class AgentB extends AbstractDedaleAgent {
 
-    class service extends OneShotBehaviour {
-        public service(AbstractDedaleAgent agent) { super(agent); }
+  public String myPosition = null;
 
-        @Override
-        public void action() {
-            DFAgentDescription dfd = new DFAgentDescription();
-            dfd.setName(getAID());
-            ServiceDescription sd = new ServiceDescription();
-            sd.setType("Explo");
-            sd.setName("AgentB");
-            dfd.addServices(sd);
-            try {
-                DFService.register(myAgent, dfd);
-                this.myAgent.addBehaviour(new whereAmI(this.myAgent));
-            } catch (FIPAException e) {
-                e.printStackTrace();
+  class service extends OneShotBehaviour {
+    public service(AbstractDedaleAgent agent) {
+      super(agent);
+    }
+
+    @Override
+    public void action() {
+      DFAgentDescription dfd = new DFAgentDescription();
+      dfd.setName(getAID());
+      ServiceDescription sd = new ServiceDescription();
+      sd.setType("Explo");
+      sd.setName(this.myAgent.getName());
+      dfd.addServices(sd);
+      try {
+        DFService.register(myAgent, dfd);
+        this.myAgent.addBehaviour(new whereAmI(this.myAgent));
+      } catch (FIPAException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  static class whereAmI extends OneShotBehaviour {
+    public whereAmI(Agent agent) {
+      super(agent);
+    }
+
+    @Override
+    public void action() {
+      Location position = (Location) ((AbstractDedaleAgent) this.myAgent).getCurrentPosition();
+      Behaviour wasat = new wasat(this.myAgent, 1000, position);
+      Behaviour walker = new randomWalk(this.myAgent);
+      this.myAgent.addBehaviour(wasat);
+      this.myAgent.addBehaviour(walker);
+      this.myAgent.addBehaviour(new watcher(this.myAgent, new ArrayList<>(Arrays.asList(walker))));
+    }
+  }
+
+  static class randomWalk extends Behaviour {
+    private final String targetPosition;
+
+    public randomWalk(Agent agent) {
+      super(agent);
+      this.targetPosition = null;
+    }
+
+    public randomWalk(Agent agent, String position) {
+      super(agent);
+      System.out.println("Target position: " + position);
+      this.targetPosition = position.trim();
+    }
+
+    @Override
+    public void action() {
+      List<Couple<Location, List<Couple<Observation, Integer>>>> lobs = ((AbstractDedaleAgent) this.myAgent).observe();
+      int moveId = 0;
+      while (true) {
+        Random r = new Random();
+        moveId = 1 + r.nextInt(lobs.size() - 1);
+        boolean safe = true;
+        for (Couple<Observation, Integer> obs : lobs.get(moveId).getRight()) {
+          if (obs.getLeft().equals(Observation.WIND)) {
+            safe = false;
+            break;
+          }
+        }
+        if (safe)
+          break;
+      }
+      ((AbstractDedaleAgent) this.myAgent).moveTo(lobs.get(moveId).getLeft());
+    }
+
+    @Override
+    public boolean done() {
+      if (this.targetPosition == null)
+        return false;
+      else {
+        return ((AbstractDedaleAgent) this.myAgent).getCurrentPosition().toString().equals(this.targetPosition);
+      }
+    }
+  }
+
+  static class watcher extends CyclicBehaviour {
+    private final ArrayList<Behaviour> victim;
+
+    public watcher(Agent agent, ArrayList<Behaviour> victims) {
+      super(agent);
+      this.victim = victims;
+    }
+
+    @Override
+    public void action() {
+      ACLMessage msg = this.myAgent.receive();
+      if (msg == null){
+        block();
+        return;
+      }
+
+      if (msg.getPerformative() == ACLMessage.INFORM) {
+        String content = msg.getContent();
+        if (content.contains("Position:") && ((AgentB) this.myAgent).myPosition == null) {
+          System.out.println("AgentB received: " + msg.getContent());
+          AgentB agent = (AgentB) this.myAgent;
+          agent.myPosition = content.split(" ")[1];
+        } 
+        if (content.contains("ACK:") && ((AgentB) this.myAgent).myPosition != null) {
+          System.out.println("ACK received, stopping behaviour");
+          this.myAgent.addBehaviour(new randomWalk(this.myAgent, ((AgentB) this.myAgent).myPosition));
+          for (Behaviour b : this.victim) {
+            this.myAgent.removeBehaviour(b);
+          }
+          this.myAgent.removeBehaviour(this);
+        }
+      }
+    }
+  }
+
+  static class wasat extends TickerBehaviour {
+    private final Location initialPosition;
+    private DFAgentDescription to = null;
+
+    public wasat(Agent agent, long period, Location position) {
+      super(agent, period);
+      this.initialPosition = position;
+    }
+
+    @Override
+    public void onTick() {
+      DFAgentDescription template = new DFAgentDescription();
+      ServiceDescription sd = new ServiceDescription();
+      sd.setType("Explo");
+      template.addServices(sd);
+      try {
+        DFAgentDescription[] result = DFService.search(this.myAgent, template);
+        if (this.to == null) {
+          for (int i = 0; i < result.length; ++i) {
+            if (result[i].getName().getName().contains("AgentA")) {
+              this.to = result[i];
+              break;
             }
+          }
         }
+        ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+        msg.addReceiver(this.to.getName());
+        String content = "Position: " + this.initialPosition;
+        if (((AgentB) this.myAgent).myPosition != null) {
+          content += " ACK: " + ((AgentB) this.myAgent).myPosition;
+        }
+        msg.setContent(content);
+        msg.setSender(this.myAgent.getAID());
+        AbstractDedaleAgent agent = (AbstractDedaleAgent) this.myAgent;
+        agent.sendMessage(msg);
+      } catch (FIPAException fe) {
+        fe.printStackTrace();
+      }
     }
+  }
 
-    static class whereAmI extends OneShotBehaviour {
-        public whereAmI(Agent agent) {
-            super(agent);
-        }
-        @Override
-        public void action() {
-            Location position = (Location) ((AbstractDedaleAgent) this.myAgent).getCurrentPosition();
-            Behaviour wasat = new wasat(this.myAgent, 1000, position);
-            this.myAgent.addBehaviour(wasat);
-            this.myAgent.addBehaviour(new watcher(this.myAgent, wasat));
-        }
-    }
+  /**
+   * This method is automatically called when "agent".start() is executed.
+   * Consider that Agent is launched for the first time.
+   * 1) set the agent attributes
+   * 2) add the behaviours
+   */
+  protected void setup() {
+    super.setup();
+    // use them as parameters for your behaviours is you want
+    List<Behaviour> lb = new ArrayList<>();
 
-    static class watcher extends CyclicBehaviour {
-        private final Behaviour victim;
-        public watcher(Agent agent, Behaviour victim){
-            super(agent);
-            this.victim = victim;
-        }
+    // ADD the initial behaviours
+    lb.add(new service(this));
 
-        @Override
-        public void action() {
-            ACLMessage msg = this.myAgent.receive();
-            if (msg != null) {
-                if (msg.getPerformative() == ACLMessage.INFORM) {
-                    String content = msg.getContent();
-                    if (content.startsWith("Position:")) {
-                        System.out.println("AgentB received: " + msg.getContent());
-                        ACLMessage reply = msg.createReply();
-                        reply.setPerformative(ACLMessage.INFORM);
-                        reply.setContent("ACK: OK");
-                        this.myAgent.send(reply);
-                        this.myAgent.addBehaviour(new randomWalk(this.myAgent, content.substring(9)));
-                    } else if (content.startsWith("ACK:")) {
-                        System.out.println("ACK received, stopping behaviour");
-                        this.myAgent.removeBehaviour(this.victim);
-                    }                
-                }
-            }
-            else {
-                System.out.println("AgentB received nothing");
-                block();
-            }
-        }
-    }
+    // MANDATORY TO ALLOW YOUR AGENT TO BE DEPLOYED CORRECTLY
+    addBehaviour(new startMyBehaviours(this, lb));
+  }
 
-    static class randomWalk extends Behaviour {
-        private final String finalPosition;
-        public randomWalk(Agent agent, String position) {
-            super(agent);
-            this.finalPosition = position.trim();
-        }
+  public String getTargetPosition() {
+    return this.myPosition;
+  }
 
-        @Override
-        public void action() {
-            List<Couple<Location, List<Couple<Observation, Integer>>>> lobs = ((AbstractDedaleAgent) this.myAgent).observe();
-            int moveId = 0;
-            while(true) {
-                Random r = new Random();
-                moveId = 1 + r.nextInt(lobs.size() - 1);
-                boolean safe = true;
-                for (Couple<Observation, Integer> obs : lobs.get(moveId).getRight()) {
-                    if (obs.getLeft().equals(Observation.WIND)) {
-                        safe = false;
-                        break;
-                    }                       
-                }
-                if (safe) break;
-            }
+  /**
+   * This method is automatically called after doDelete()
+   */
+  protected void takeDown() {
+    super.takeDown();
+  }
 
-            ((AbstractDedaleAgent) this.myAgent).moveTo(lobs.get(moveId).getLeft());
-        }
+  /**
+   * This method is automatically called before migration.
+   * You can add here all the saving you need
+   */
+  protected void beforeMove() {
+    super.beforeMove();
+  }
 
-
-        @Override
-        public boolean done() {
-            return ((AbstractDedaleAgent) this.myAgent).getCurrentPosition().toString().equals(this.finalPosition);
-        }
-
-        public int onEnd() {
-            System.out.println("AgentB reached final position");
-            return 0;
-        }
-    }
-
-    static class wasat extends TickerBehaviour {
-        private final Location initialPosition;
-        public wasat(Agent agent, long period, Location position) {
-            super(agent, 1000);
-            this.initialPosition = position;
-        }
-
-        @Override
-        public void onTick() {
-            DFAgentDescription template = new DFAgentDescription();
-            ServiceDescription sd = new ServiceDescription();
-            sd.setType("Explo");
-            template.addServices(sd);
-            try {
-                DFAgentDescription[] result = DFService.search(myAgent, template);
-                System.out.println("Found the following agents:");
-                DFAgentDescription target = null;
-                for (int i = 0; i < result.length; ++i) {
-                    System.out.println(result[i].getName().getName());
-                    if (result[i].getName().getName().contains("AgentA")) {
-                        target = result[i];
-                        break;
-
-                    }
-                }
-                ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
-                msg.addReceiver(target.getName());
-                msg.setContent("Position: " + initialPosition);
-                myAgent.send(msg);
-            } catch (FIPAException fe) {
-                fe.printStackTrace();
-            }
-        }
-    }
-
-    /**
-     * This method is automatically called when "agent".start() is executed.
-     * Consider that Agent is launched for the first time.
-     * 1) set the agent attributes
-     * 2) add the behaviours
-     */
-    protected void setup() {
-        super.setup();
-        //use them as parameters for your behaviours is you want
-        List<Behaviour> lb = new ArrayList<>();
-
-
-        // ADD the initial behaviours
-        lb.add(new service(this));
-
-        // MANDATORY TO ALLOW YOUR AGENT TO BE DEPLOYED CORRECTLY
-        addBehaviour(new startMyBehaviours(this, lb));
-    }
-
-    /**
-     * This method is automatically called after doDelete()
-     */
-    protected void takeDown() {
-        super.takeDown();
-    }
-
-    /**
-     * This method is automatically called before migration.
-     * You can add here all the saving you need
-     */
-    protected void beforeMove() {
-        super.beforeMove();
-    }
-
-    /**
-     * This method is automatically called after migration to reload.
-     * You can add here all the info regarding the state you want your agent to restart from
-     */
-    protected void afterMove() {
-        super.afterMove();
-    }
+  /**
+   * This method is automatically called after migration to reload.
+   * You can add here all the info regarding the state you want your agent to
+   * restart from
+   */
+  protected void afterMove() {
+    super.afterMove();
+  }
 }
